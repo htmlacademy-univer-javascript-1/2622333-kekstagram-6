e16f98cc4d429782ea160321b4857c6962bcfe22
Екатерина Пьянкова <pyankovaa06@mail.ru>
2025-10-10 03:14:48 +0500

    Добавляет файл с заданием

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,45 @@
 /* eslint-disable no-unused-vars */
-function getLength(string, length) {
-  return string.length <= length;
+//4
+function timeToMinutes(timeStr) {
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return hours * 60 + minutes;
 }
 
-function isPalindrome(string) {
-  const normalizedString = string.toLowerCase().replaceAll(' ', '');
-  const reversedString = normalizedString.split('').reverse().join('');
-  return reversedString === normalizedString;
+function isMeetingOnWorkTime(workStart, workEnd, meetingStart, meetingDuration) {
+  const workStartMinutes = timeToMinutes(workStart);
+  const workEndMinutes = timeToMinutes(workEnd);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStartMinutes &&
+          meetingEndMinutes <= workEndMinutes;
 }
 
-function isNumber(phrase) {
-  let result = '';
-  const string = phrase.toString().replaceAll(' ', '');
-  for (let i = 0; i < string.length; i++) {
-    const digit = Number(string[i]);
-    if ((result.length === 0) & (digit === 0)) {
-      continue;
-    } if (!Number.isNaN(digit)) {
-      result += digit;
-    }
-  }
-  return result.length === 0 ? NaN : result;
-}
+// //1
+// function getLength(string, length) {
+//   return string.length <= length;
+// }
+
+// //2
+// function isPalindrome(string) {
+//   const normalizedString = string.toLowerCase().replaceAll(' ', '');
+//   const reversedString = normalizedString.split('').reverse().join('');
+//   return reversedString === normalizedString;
+// }
+
+// //3
+// function isNumber(phrase) {
+//   let result = '';
+//   const string = phrase.toString().replaceAll(' ', '');
+//   for (let i = 0; i < string.length; i++) {
+//     const digit = Number(string[i]);
+//     if ((result.length === 0) & (digit === 0)) {
+//       continue;
+//     } if (!Number.isNaN(digit)) {
+//       result += digit;
+//     }
+//   }
+//   return result.length === 0 ? NaN : result;
+// }
+
+
